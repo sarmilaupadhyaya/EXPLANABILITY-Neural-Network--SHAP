587ffd1ce4d124a90f68ddb8355a7e7f7ba702b1
Sharmila Upadhyaya <sarmilaupadhyaya43@gmail.com>
2021-03-31 23:58:30 +0200

    code to analyse the data

--- src/dataAnalysis.py
+++ src/dataAnalysis.py
@@ -1,5 +1,5 @@
 import os
-
+import pickle
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -38,10 +38,31 @@ def clean_text(text):
     text = ' '.join([word for word in text.split() if word not in (stop_words)])    ### Stopwords removal
     return text
 
+def save_tokenizer(wordindex):
 
+    with open("data/wordindex","wb") as f:
+        pickle.dump(wordindex, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+    print("word index saved")
+
+def load_tokenizer(word_index):
+
+    with open(word_index, "rb") as f:
+            tokenizer  = pickle.load(f)
+
+    return tokenizer
+
+
+
+def get_data_all_label(train):
+
+    df = train[(train.toxic == 1) & (train.severe_toxic==1) & (train.obscene==1) & (train.threat==1) & (train.insult==1)&(train.identity_hate==1)]
+    print("Comments that has all labels:")
+    print("\n".join(df["comment_text"].tolist()))
 
 def data_analysis(train):
 
+    get_data_all_label(train)
     categorywise_data = train.drop(['id', 'comment_text'], axis=1)
     counts_category = []                       
     categories = list(categorywise_data.columns.values)
@@ -50,10 +71,14 @@ def data_analysis(train):
 
     dataframe = pd.DataFrame(counts_category, columns=['Labels', 'number_of_comments'])   
 
-    dataframe.plot(x='Labels', y='number_of_comments', kind='bar',figsize=(8,8))
-    plt.title("Number of comments per category")
-    plt.ylabel('No. of Occurrences', fontsize=12)
-    plt.xlabel('Labels', fontsize=12)
+    dataframe.plot(x='Labels', y='number_of_comments', kind='bar',figsize=(10,8), color="red")
+    plt.title("Number of comments per category", fontsize=20)
+    plt.ylabel('No. of Occurrences', fontsize=20)
+    plt.xlabel('Labels', fontsize=20)
+    plt.xticks(size=20)
+    plt.yticks(size=20)
+    plt.tight_layout()
+    plt.show()
 
     dataframe = pd.DataFrame(pd.DataFrame(train[train.columns[2:]].sum(axis=1)).reset_index()[0].value_counts())
     dataframe["Total no. of sentences"]=dataframe[0]
@@ -62,6 +87,8 @@ def data_analysis(train):
     plt.title("No of comments based on the count of labels")
     plt.ylabel('Total no. of sentences', fontsize=12)
     plt.xlabel('Total No. of labels in a sentence', fontsize=12)
+    plt.tight_layout()
+    plt.show()
 
     fig, plots = plt.subplots(2,3,figsize=(15,12))
     plot1, plot2, plot3, plot4, plot5, plot6 = plots.flatten()
@@ -71,10 +98,23 @@ def data_analysis(train):
     sns.countplot(train['identity_hate'], ax = plot4)
     sns.countplot(train['toxic'], ax = plot5)
     sns.countplot(train['severe_toxic'], ax = plot6)
+    plt.show()
+    target_data = train.drop(['id', 'comment_text'], axis=1)
+    corrMatrix = target_data.corr()
+    sns.heatmap(corrMatrix, annot=True)
+    plt.xticks(size=20)
+    plt.yticks(size=20)
+    plt.tight_layout()
+    plt.show()
 
-def main(train_path, analysis=False):
+
+
+def main(train_path, analysis=False, word_index=None, huaodata=None):
     
     train = read_csv(train_path)
+    if huaodata is not None:
+        huaodata = read_csv(huaodata)
+        
 
     train_data = train["comment_text"]
     train_label=train[['toxic', 'severe_toxic', 'obscene', 'threat','insult', 'identity_hate']]
@@ -86,18 +126,16 @@ def main(train_path, analysis=False):
     tokenizer = Tokenizer(num_words = 40000) #40000 words are used here
     tokenizer.fit_on_texts(train_data)
 
-    #convert each text into array of integers with help of tokenizer.
+    if word_index is None:
+        save_tokenizer(tokenizer)
+    else:
+        tokenizer = load_tokenizer(word_index)
+
     train_final = tokenizer.texts_to_sequences(train_data)
     train_padded =pad_sequences(train_final, maxlen=150)
 
+    if huaodata is not None:
+        comment_text_ha = pad_sequences(tokenizer.texts_to_sequences(huaodata["comment_text"]), maxlen=150)
+        label_ha = huaodata[['toxic', 'severe_toxic', 'obscene', 'threat','insult', 'identity_hate']]
 
-    return train_padded, train_label, tokenizer.word_index
-
-
-
-#main("data/train.csv","data/test.csv", True)
-
-
-
-
-
+    return train_padded, train_label, tokenizer.word_index, comment_text_ha, label_ha
